d2a4dcf0ca460ed5ce093ba0304c3219ee896224
엄준일 <umc@ntreev.com>
2019-03-15 10:59:23 +0900

    gulp-chmod 처리 추가

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,28 @@
 const gulp = require("gulp");
+const chmod = require("gulp-chmod");
 const tsconfig = require("./tsconfig.json");
+const package = require("./package");
 
 const outDir = tsconfig.compilerOptions.outDir;
 
 gulp.task("copy-files", (cb) => {
-    gulp.src(["config.json", "package.json"])
-        .pipe(gulp.dest(outDir));
+  gulp.src(["config.json", "package.json"])
+    .pipe(gulp.dest(outDir));
 
-    cb();
+  cb();
 });
+
+gulp.task("chmod", (cb) => {
+  const src = package.bin["powerjudge-api-server"];
+  gulp.src(src)
+    .pipe(chmod(0o755));
+
+  cb();
+});
+
+gulp.task("default", gulp.parallel(
+  [
+    "copy-files",
+    "chmod"
+  ]
+));
