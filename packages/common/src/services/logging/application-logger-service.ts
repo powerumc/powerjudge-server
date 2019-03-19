cb3ed5bc13da09b094031aab851813855516827c
엄준일 <umc@ntreev.com>
2019-03-19 11:18:06 +0900

    nestjs 로그 코드 제거

--- packages/common/src/services/logging/application-logger-service.ts
+++ packages/common/src/services/logging/application-logger-service.ts
@@ -30,26 +30,21 @@ export class ApplicationLoggerService extends Logger {
 
   debug(message: string): void {
     this.logger.debug(message);
-    super.log(message);
   }
 
   warn(message: string): void {
     this.logger.warn(message);
-    super.warn(message);
   }
 
   info(message: string): void {
     this.logger.info(message);
-    super.log(message);
   }
 
   error(message: string): void {
     this.logger.error(message);
-    super.error(message);
   }
 
   critical(message: string): void {
     this.logger.crit(message);
-    super.error(message);
   }
 }
