e30ab72a3cbefc515335613e9e86c58bbadec201
엄준일 <umc@ntreev.com>
2019-03-22 12:34:25 +0900

    api 패키지에 DockerService 제거

--- packages/api/src/services/services-module.ts
+++ packages/api/src/services/services-module.ts
@@ -1,12 +1,10 @@
 import {Module} from "@nestjs/common";
 import {ApplicationBootstrapperService, ApplicationConfigurationService} from "./configurations";
-import {DockerService} from "./docker";
 import {CommonServicesModule} from "powerjudge-common";
 
 const services = [
   ApplicationConfigurationService,
-  ApplicationBootstrapperService,
-  DockerService
+  ApplicationBootstrapperService
 ];
 
 @Module({
