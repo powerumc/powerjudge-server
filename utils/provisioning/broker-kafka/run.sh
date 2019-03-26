82ae1afed1c32533eb27fd3bbbe67b2766222e2a
엄준일 <umc@ntreev.com>
2019-03-26 17:58:59 +0900

    kafka 설정 부분에서 계속 문제 보고 있음

--- utils/provisioning/broker-kafka/run.sh
+++ utils/provisioning/broker-kafka/run.sh
@@ -6,9 +6,11 @@ echo "powerjudge version ${VERSION}"
 docker run \
 --name powerjudge-broker-kafka \
 -p 9092:9092 \
+-e KAFKA_BROKER_ID="1" \
 -e KAFKA_ZOOKEEPER_CONNECT="zookeeper:2181" \
 -e KAFKA_ADVERTISED_HOST_NAME="localhost" \
 -e KAFKA_ADVERTISED_PORT="9092" \
+-e KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://localhost:9092" \
 -e KAFKA_LOG_DIRS="/pj/kafka/log" \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v /pj/kafka/data:/pj/kafka/data \
