0f937769599fb60e83ad26e83d591f922448eb02
Junil Um <powerumc@gmail.com>
2019-03-28 00:08:33 +0900

    mono, gcc Docker 컴파일러 추가

--- utils/provisioning/compilers/build.sh
+++ utils/provisioning/compilers/build.sh
@@ -4,4 +4,7 @@ VERSION=$(cat ../../../package.json | jq -r '.version')
 echo "powerjudge version ${VERSION}"
 
 docker build -f mono/Dockerfile -t powerjudge/powerjudge-compiler-mono:${VERSION} .
+docker tag powerjudge/powerjudge-compiler-mono:${VERSION} powerjudge/powerjudge-compiler-mono:latest
+
 docker build -f gcc/Dockerfile -t powerjudge/powerjudge-compiler-gcc:${VERSION} .
+docker tag powerjudge/powerjudge-compiler-gcc:${VERSION} powerjudge/powerjudge-compiler-gcc:latest
